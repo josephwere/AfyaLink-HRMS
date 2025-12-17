c888f0c48d31951c705d08b5f39acda4a18315a3
josephwere <josephogwe8@gmail.com>
2025-12-17 13:46:37 +0300

    Delete frontend directory

--- frontend/src/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Navbar(){
-  return (
-    <header className="navbar">
-      <div className="brand">AfyaLink HRMS 2.0</div>
-      <div className="nav-actions">Premium • Secure • AI-assisted</div>
-    </header>
-  );
-}
