d51d5874b32ecb4422f0d8979b4d5203f179104d
josephwere <josephogwe8@gmail.com>
2025-12-30 02:33:42 +0300

    Update verificationReminders.js

--- backend/workers/verificationReminders.js
+++ backend/workers/verificationReminders.js
@@ -1,36 +1,26 @@
 import User from "../models/User.js";
 import { sendEmail } from "../utils/mailer.js";
+import AuditLog from "../models/AuditLog.js";
 
-export const sendVerificationReminders = async () => {
-  const now = new Date();
+...
 
-  const users = await User.find({
-    emailVerified: false,
-    verificationDeadline: { $gt: now },
-  });
+await sendEmail({
+  to: user.email,
+  subject: "Verify your AfyaLink account",
+  html: `
+    <p>Your account will be deleted in ${tag}.</p>
+    <p>Please verify your email to keep your profile.</p>
+  `,
+});
 
-  for (const user of users) {
-    const msLeft = user.verificationDeadline - now;
-    const hoursLeft = msLeft / (1000 * 60 * 60);
+user.verificationRemindersSent.push(tag);
+await user.save();
 
-    let tag = null;
-
-    if (hoursLeft <= 2) tag = "2h";
-    else if (hoursLeft <= 72) tag = "3d";
-    else if (hoursLeft <= 336) tag = "14d";
-
-    if (!tag || user.verificationRemindersSent.includes(tag)) continue;
-
-    await sendEmail({
-      to: user.email,
-      subject: "Verify your AfyaLink account",
-      html: `
-        <p>Your account will be deleted in ${tag}.</p>
-        <p>Please verify your email to keep your profile.</p>
-      `,
-    });
-
-    user.verificationRemindersSent.push(tag);
-    await user.save();
-  }
-};
+await AuditLog.create({
+  actorId: user._id,
+  actorRole: user.role || "user",
+  action: "VERIFICATION_REMINDER_SENT",
+  resource: "User",
+  resourceId: user._id,
+  metadata: { reminder: tag },
+});
