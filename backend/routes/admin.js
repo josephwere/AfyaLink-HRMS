5bbfb3ec05683511c0831d8dcbbea317604a02c7
josephwere <josephogwe8@gmail.com>
2025-12-30 04:47:04 +0300

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,22 @@
-import { exportExpiringUsersCSV } from "../controllers/adminVerificationController.js";
+import { exportExpiringUsersCSV } from "../controllers/adminVerificationController.js"
+import express from "express";
+import { createAdmin } from "../controllers/adminController.js";
+import auth from "../middleware/auth.js";
+import { allowRoles } from "../middleware/roles.js";
 
+const router = express.Router();
+
+/**
+ * SUPER_ADMIN → Create Admin
+ */
+router.post(
+  "/create",
+  auth,
+  allowRoles("SUPER_ADMIN"),
+  createAdmin
+);
+
+export default router;
 router.get(
   "/export/unverified-users",
   auth,
